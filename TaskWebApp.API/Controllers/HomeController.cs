fb483104f4ff81396d8e7321ba4b4de962ccac3f
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-18 20:06:50 +0300

    new api project

---
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        public async Task<IActionResult> GetContentAsync()
+        {
+
+            var myTask = new HttpClient().GetStringAsync("http://www.google.com");
+
+            var data = await myTask;
+
+            return Ok(data);
+
+        }
+
+
+    }
+}
