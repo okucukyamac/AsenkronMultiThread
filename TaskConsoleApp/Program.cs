97c7e0bc6c215eb3bdd7a94de5bbf8471d7cc7d1
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-18 23:29:59 +0300

    .

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TaskConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((data) =>
+            {
+                Console.WriteLine(data.Result.Length);
+            });
+        }
+    }
+}
