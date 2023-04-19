1d5f1e932193902a615b4a6108747f2314bd0c5e
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-19 18:31:07 +0300

    whenall

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,35 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskConsoleApp
 {
+
+    public class Content
+    {
+        public string Site { get; set; }
+        public int Len { get; set; }
+    }
     internal class Program
     {
         private async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            List<string> urlList = new List<string>
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.amazon.com",
+                "https://www.n11.com",
+                "https://www.haberturk.com",
+            };
 
-            var mytask = new HttpClient().GetStringAsync("https://www.google.com");
+            List<Task<Content>> taskList= new List<Task<Content>>();
 
-            Console.WriteLine("Arada yapılacak işler.");
+            urlList.ToList().ForEach(url =>
+            {
+                taskList.Add(GetContentAsync(url));
 
-            var data = await mytask;
+            });
 
-            Console.WriteLine(data.Length.ToString());
+            var contents = await Task.WhenAll(taskList.ToArray());
+
+            contents.ToList().ForEach((content) =>
+            {
+                Console.WriteLine(content.Len);
+            });
+
+        }
 
 
-            //Console.WriteLine("Hello World!");
+        public static async Task<Content> GetContentAsync(string url)
+        {
+            Content c = new Content();
+            var data = await new HttpClient().GetStringAsync(url);
 
-            //var mytask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((data) =>
-            //{
-            //    Console.WriteLine(data.Result.Length);
-            //});
+            c.Site = url;
+            c.Len = data.Length;
 
-            //Console.WriteLine("Arada yapılacak işler.");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
 
-            //await mytask;
+            return c;
 
         }
     }
+
+    
 }
